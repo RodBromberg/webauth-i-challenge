1bab89f876c4ad85fa78b3a2a8c09b1f2397dd91
RodBromberg <rod.bromberg@yahoo.com>
2019-06-09 19:29:22 -0400

    mvp

--- server.js
+++ server.js
@@ -4,14 +4,67 @@ const cors = require('cors');
 const bcrypt = require('bcryptjs')
 const Users = require('./data/dbMethods')
 const server = express();
+const session = require('express-session')
+const SessionStore = require('connect-session-knex')(session)
+const authRouter = require('./auth/restricted-middleware')
+
+const sessionConfig = {
+    name: 'monkey', // sid,
+    secret: 'Keep it secret, stupid!',
+    cookie: {
+        maxAge: 1000 * 10 * 60,
+        secure: false,
+        httpOnly: true,
+    },
+    resave: false,
+    saveUninitialized: false, // GDPR laws against setting cookies automatically
+    store: new SessionStore({
+        knex: require('./data/dbConfig.js'),
+        tablename: 'sessions',
+        sidfieldname: 'sid',
+        createtable: true,
+        clearInterval: 60 * 60 * 1000 // 1 hour
+    })
+}
+
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig))
 
-server.get('/', (req, res) => {
-    res.send("yes!");
+const restricted = (req, res, next) => {
+    const { username, password } = req.headers;
+
+    if (req.session && req.session.user) {
+        next();
+    } else {
+        res.status(418).json({ message: 'No credentials provided' });
+    }
+};
+
+server.get('/api/users', restricted, (req, res) => {
+    Users.get()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => res.send(err));
 });
+
+
+// server.use('/api/users', authRouter)
+
+// server.get("/api/users", async(req, res) => {
+//     //get the cohorts from database
+//     try {
+//         //Not using db helper functions
+//         const users = await Users.get()
+//         res.status(200).json(users);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// })
+
 
 server.post("/api/register", (req, res) => {
     const user = req.body;
@@ -31,30 +84,46 @@ server.post("/api/register", (req, res) => {
     }
 });
 
+
+server.post('/ap')
+
+
 //fds
+
+// server.get("/api/users", async(req, res) => {
+//     try {
+//         const getUsers = await Users.get();
+//         // sets the current users to a variable getUsers
+//         // when getUsers is empty its falsy and wont execute the if statement
+//         // when values are sent through its true
+//         if (getUsers) {
+//             res.status(200).json(getUsers);
+//         } else {
+//             res.status(400).json({ message: 'cant get users' });
+//         }
+//     } catch (err) {
+//         res.status(500).json({ message: "Error." });
+//     }
+// });
+
 
 server.get("/api/users", async(req, res) => {
     try {
-        const getUsers = await Users.get();
-        // sets the current users to a variable getUsers
-        // when getUsers is empty its falsy and wont execute the if statement
-        // when values are sent through its true
-        if (getUsers) {
-            res.status(200).json(getUsers);
-        } else {
-            res.status(400).json({ message: 'cant get users' });
-        }
+        const users = await Users.get();
+        res.status(200).json(users);
     } catch (err) {
-        res.status(500).json({ message: "Error." });
+        res.status(500).json(err);
     }
 });
+
 server.post('/api/login', (req, res) => {
     let { username, password } = req.body;
 
     Users.login({ username })
         .first()
         .then(user => {
-            if (user) {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                req.session.user = user;
                 res.status(200).json({ message: `Welcome ${user.username}!` });
             } else {
                 res.status(401).json({ message: 'Invalid Credentials' });
@@ -65,5 +134,18 @@ server.post('/api/login', (req, res) => {
         });
 });
 
+server.get('/api/logout', restricted, (req, res) => {
+    if (req.session) {
+        req.session.destroy(err => {
+            if (err) {
+                res.json({ message: 'error', err })
+            } else {
+                res.status(200).json({ message: 'success logging out' })
+            }
+        })
+    } else {
+        res.status(500).json({ message: 'couldnt happpen' })
+    }
+})
 
 module.exports = server
